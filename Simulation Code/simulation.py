540fd154f9d2c24f73c87c2253b53d69c053b0ca
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2020-04-07 21:21:39 -0700

    notes

--- Simulation Code/simulation.py
+++ Simulation Code/simulation.py
@@ -9,6 +9,18 @@ import serial as ser
 
 def setup():
     # Simulation setup here
+
+	'''
+	"In space vehicles, one can find multiple
+	sources of disturbances, such as position or velocity measuring errors,
+	thruster misalignments, or even atmospheric drag"
+
+	source: https://www.sciencedirect.com/science/article/abs/pii/S0967066111001985
+
+	These things will be our process uncertainty, v(k). If we care enough to research
+	how much uncertainity this will correspond to numerically, we can do that. Otherwise
+	we can mess with the numerics.
+	'''
     pass
 
 
