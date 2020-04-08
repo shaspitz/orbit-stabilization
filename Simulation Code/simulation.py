f4490fb3a5e4361f24c5c67eb10911df0dec8c7a
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2020-04-08 10:35:16 -0700

    comments

--- Simulation Code/simulation.py
+++ Simulation Code/simulation.py
@@ -10,6 +10,8 @@ import serial as ser
 def setup():
     # Simulation setup here
 
+    # Added some comments below about things we should think about
+
 	'''
 	"In space vehicles, one can find multiple
 	sources of disturbances, such as position or velocity measuring errors,
@@ -20,6 +22,16 @@ def setup():
 	These things will be our process uncertainty, v(k). If we care enough to research
 	how much uncertainity this will correspond to numerically, we can do that. Otherwise
 	we can mess with the numerics.
+	'''
+
+	'''
+	How would we make a space rendevous a 2D problem? its about launch timing, see: https://www.youtube.com/watch?v=oNXPtZDS-cg
+	'''
+
+	'''
+	Problem could be sattellite rendevous instead of just traj stabilization? Could formulate problem as regulating the chaser
+	sattellites orbit to be the same as the target sattellite. Then would use "V-bar approach" to increase radial velocity along
+	 the target orbit until proximity is reached.
 	'''
     pass
 
