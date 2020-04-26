da8703002475ec706b4960a4ed2a4113abd99818
Muireann <muireann@berkeley.edu>
2020-04-26 14:20:08 -0700

    changed to discretized

--- Simulation Code/simulation.py
+++ Simulation Code/simulation.py
@@ -61,19 +61,24 @@ Could formulate problem as regulating the chaser sattellites orbit to be the
 same as the target sattellite. Then would use "V-bar approach" to increase
 radial velocity along the target orbit until proximity is reached.
 '''
-def linear_dyn(r0,w0):
-
+def noncont_linear_dyn(r0,w0):
+    #discretisation obtained from http://users.wpi.edu/~zli11/teaching/rbe595_2017/LectureSlide_PDF/discretization.pdf
     A = np.array([[0, 1, 0, 0],
                   [3*w0**2, 0, 0, 2*r0*w0],
                   [0, 0, 0, 1],
                   [0, -2*w0/r0, 0, 0]])
+    print(np.linalg.inv(A))
 
     B = np.array([[0, 0],
                   [1, 0],
                   [0, 0],
                   [0, 1/r0]])
+    G=scipy.linalg.expm(A)
+    H=(scipy.linalg.expm(A) - np.eye(4))@ B @ np.linalg.inv(A)
+    print(G)
+    print(H)
 
-    return A, B
+    return G,H
 
 #kalman Filter
 #A:State update
@@ -129,7 +134,7 @@ def nl_dyn_cont(t, x, u=np.zeros(2)):
     # testing
     # x_out = np.zeros(4)
 
-    '''
+    ''' 
     # add new position to path array
     path = np.append(path, np.array([x_sat, y_sat]))
     '''
@@ -163,37 +168,9 @@ def lin_dyn_cont(t, x, r0, u=np.zeros((2, 1))):
     B = np.array([[0, 0],
                   [1, 0],
                   [0, 0],
-                  [0, 1/r0]])
+                  [0, 1/r0]]) 
 
     return (A @ x + B @ u).ravel()  # output 1-d array again
-
-#kalman Filter
-#A:State update
-#V:process noise
-#W:sensor noise
-#z:measurements
-#K:gain matrix at last timestep
-
-
-def KF(A,H,V,W,z,Pm,K,XM):
-    #prior update
-    XP = A @ XM
-    Pp = A @ Pm @ A.T + V
-    #measurement update
-    K = Pp @ H.T @ (np.linalg.inv(H @ Pp @ H.T + W))
-    XM = XP + K @ (z-H @ XP)
-    Pm = (np.eye(4) - K @ H) @ Pp @ (np.eye(4) - K @ H).T + K @ W @ K.T
-    return K,Pm
-
-def LQR(A,B,N,S,Q,R):
-    U = np.zeros((4,4,N+1))
-    #Initialize the Ricatti equation with U(N) = S
-    U[:,:,N] = S
-    #Iterate backwards to compute U(k)
-    for k in range(N):
-        U[:,:,N-k-1] = Q + A.T @ (U[:,:,N-k]) @ (A)- (A.T) @ (U[:,:,N-k]) @ (B) @ (np.linalg.inv(R+(B.T) @ (U[:,:,N-k]) @ (B))) @ (B.T) @ (U[:,:,N-k]) @ (A)
-        # print(U[:,:,N-k-1])
-    return U
 
 
 def main():
@@ -229,10 +206,7 @@ def main():
         sys_sol_lin = solve_ivp(lin_dyn_cont, [t_sim[0], t_sim[-1:]], x0,
                                 method='RK45', t_eval=t_sim, args=(r0, u))
 
-<<<<<<< Updated upstream
-=======
-        #print(sys_sol_lin.y[2], w0)
->>>>>>> Stashed changes
+
         # Simulate continuous nonlinear system (yes it does run, not quickly)
         '''
         t_sim = np.linspace(0, 10, 100)
@@ -240,35 +214,7 @@ def main():
         sys_sol_nl = solve_ivp(nl_dyn_cont, [t_sim[0], t_sim[-1:]], x0,
                             method='RK45', t_eval=t_sim)
         '''
-<<<<<<< Updated upstream
-        #use the output from the IVP as the measurements Z for the KF and LQR
-        N = len(t_sim)
-        x = np.zeros((4,N))
-        Pm = np.eye(4)
-        u = np.zeros((2,N-1))
-
-        #initialize matrices for LQG
-        #NEED TUNING
-        H = np.eye(4)
-        V = np.eye(4)
-        W = np.eye(4)
-        Q = np.eye(4)
-        K = np.zeros((4,4))
-        S = np.array([[234,56,5,454],[3,6,4,2],[765,35,76,53],[536,765,3,23]])
-        R = np.eye(2)
-        A,B = linear_dyn(r0,w0)
-        U = LQR(A,B,N,S,Q,R)
-        for i in range(1,N):
-            K,Pm = KF(A,H,V,W,sys_sol_lin.y[:,i-1],Pm,K,x[:,i-1])
-            #calculate control input for each time step
-            u[:,i-1] = -(np.linalg.inv(R+B.T @ (U[:,:,i-1]) @ (B))) @ (B.T) @ (U[:,:,i-1]) @ (A) @ (x[:,i-1])
-            # print(u[:,i-1])
-            #calculate state at each time step
-            x[:,i]=A @ x[:,i-1] + B @ u[:,i-1] + K @ ( sys_sol_lin.y[:,i-1] - H @ (A @ x[:,i-1] + B @ u[:,i-1]))
-
-=======
         
->>>>>>> Stashed changes
         # solution will be sys_sol.y with [0:3] being arrays of state
         # print('last 10 values of radius:', sys_sol.y[0][sys_sol_lin.y[i]:-10])
         
@@ -297,12 +243,12 @@ def main():
         K = np.zeros((4,4))
         S = np.array([[42200000,0,0,0],[0,0,0,0],[0,0,4.85,0],[0,0,0,4.85360589*(10**(-5))]])
         R_m = np.eye(2)
-        A,B = linear_dyn(r0,w0)
+        A,B = noncont_linear_dyn(r0,w0)
         U = LQR(A,B,N,S,Q,R_m)
         for i in range(1,N):
             K,Pm = KF(A,H,V,W,sys_sol_lin.y[:,i-1]+np.random.normal(0,W),Pm,K,x[:,i-1])
             #calculate control input for each time step
-            u[:,i-1] = -(np.linalg.inv(R+B.T @ (U[:,:,i-1]) @ (B))) @ (B.T) @ (U[:,:,i-1]) @ (A) @ (x[:,i-1])
+            u[:,i-1] = -(np.linalg.inv(R_m+B.T @ (U[:,:,i-1]) @ (B))) @ (B.T) @ (U[:,:,i-1]) @ (A) @ (x[:,i-1])
             # print(u[:,i-1])
             #calculate state at each time step
             x[:,i]=A @ x[:,i-1] + B @ u[:,i-1] + K @ ( sys_sol_lin.y[:,i-1] - H @ (A @ x[:,i-1] + B @ u[:,i-1]))
@@ -310,13 +256,10 @@ def main():
         #using states from LQG
         x_sat_KF=[x[0][i]*np.cos(x[2][i]) for i in range(len(t_sim))]
         y_sat_KF=[x[0][i]*np.cos(x[2][i]) for i in range(len(t_sim))]
-<<<<<<< Updated upstream
         #x_sat_lin = [sys_sol_lin.y[0][i]*np.cos(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
-=======
         #print(x_sat_KF)
         #x_sat_lin = [sys_sol_lin.y[0][i]*np.cos(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
         #print(x_sat_lin)
->>>>>>> Stashed changes
         #y_sat_lin = [sys_sol_lin.y[0][i]*np.sin(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
         # x_sat_nl = [sys_sol_nl.y[0][i]*np.cos(sys_sol_nl.y[2][i]) for i in range(len(t_sim))]
         # y_sat_nl = [sys_sol_nl.y[0][i]*np.sin(sys_sol_nl.y[2][i]) for i in range(len(t_sim))]
