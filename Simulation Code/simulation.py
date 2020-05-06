38f2f903e7fb56b097ff46492390c9552cd9b146
Muireann <muireann@berkeley.edu>
2020-05-05 18:02:12 -0700

    working LQG

--- Simulation Code/simulation.py
+++ Simulation Code/simulation.py
@@ -8,7 +8,6 @@ import threading
 import serial
 import scipy
 from scipy.integrate import solve_ivp
-from scipy.signal import cont2discrete as c2d
 from scipy.linalg import solve_discrete_are as DARE
 
 # Setup global variables
@@ -58,28 +57,6 @@ Could formulate problem as regulating the chaser sattellites orbit to be the
 same as the target sattellite. Then would use "V-bar approach" to increase
 radial velocity along the target orbit until proximity is reached.
 '''
-def discrete_linear_dyn(r0,w0,Ts):
-    #discretisation obtained from http://users.wpi.edu/~zli11/teaching/rbe595_2017/LectureSlide_PDF/discretization.pdf
-    A = np.array([[0, 1, 0, 0],
-                  [3*w0**2, 0, 0, 2*r0*w0],
-                  [0, 0, 0, 1],
-                  [0, -2*w0/r0, 0, 0]])
-
-
-    B = np.array([[0, 0],
-                  [1, 0],
-                  [0, 0],
-                  [0, 1/r0]])
-    
-    # Golden Lecutrue Note: P. 34
-    C = np.array([
-        [1/r0, 0, 0, 0],
-        [0   , 0, 1, 0]
-        ])
-
-    # https://docs.scipy.org/doc/scipy-0.15.1/reference/generated/scipy.signal.cont2discrete.html
-    Ad, Bd, Cd, Dd, dtime = c2d((A, B, C, 0), Ts, method='zoh')
-    return Ad,Bd
 
 def nl_dyn_cont(t, x, u=np.zeros(2)):
     # Continuous nonlinear dynamics of sattellite system (uses 1-d nparrays)
@@ -143,12 +120,23 @@ def lin_dyn_discrete(x, r0, Ts, u=np.zeros((2, 1))):
     xnext = x + Ts*dx
     return xnext
 
+def dyn_discrete(r0,w0,Ts):
+    '''
+    Discrete dynamics using forward Euler method
+    '''
+    A = np.array([[1, Ts, 0, 0],
+                  [3*Ts*w0**2, 1, 0, 2*Ts*r0*w0],
+                  [0, 0, 1, Ts],
+                  [0, -2*Ts*w0/r0, 0, 1]])
+   
+    B=np.array([[0,0],[1+Ts,0],[0,0],[0,(1+Ts)/r0]])
+    return A,B
 
 def equil(t):
     '''
     For adding equil trajectory back to solution
     '''
-    return np.array([r0, 0, w0*t, w0])
+    return np.array([r0,0,w0*t,w0])
 
 
 def input_command():
@@ -287,8 +275,9 @@ def main():
         # Simulations
 
         # Simulate continuous linearized system
-        t_sim = np.linspace(0, 100000, 100)
-        Ts=t_sim[-1]/len(t_sim)
+        t_sim = np.linspace(0, 100000, 100000)
+        Ts = t_sim[-1] / len(t_sim)
+
         # Intial conditions, remember this is deviation from equil in polar
         x0 = [0, 0, 0, 0]
         u = np.zeros((2, 1))
@@ -303,19 +292,12 @@ def main():
                             method='RK45', t_eval=t_sim)
         '''
 
-        # solution will be sys_sol.y with [0:3] being arrays of state
-        # print('last 10 values of radius:', sys_sol.y[0][:-10])
-
         # Add equil trajectory back to solution
         def equil(t):
             return np.array([r0, 0, w0*t, w0])
-
-        for i in range(len(sys_sol_lin.y)):
-            sys_sol_lin.y[i] = np.array(
-                [sys_sol_lin.y[i][j]
-                 + equil(t_sim[j])[i] for j in range(len(t_sim))])
-            
-        #implement steady state LQG          
+#         
+                #implement steady state LQG
+        A,B = dyn_discrete(r0,w0,Ts)
         R_m = np.eye(2)
         Q = np.eye(4)
         H = np.eye(4)
@@ -323,42 +305,58 @@ def main():
         W = np.eye(4)
         N = len(t_sim)
         x = np.zeros((4,N))
-        x[:,0]=np.array([r0, 0, w0, w0])
         xhat = np.zeros((4,N))
-        for k in range(1,N-1):
-            A,B=discrete_linear_dyn(x[0,k-1],x[2,k-1],Ts)
-            Uinf = DARE(A, B, Q, R_m)
-            Pinf = DARE(A.T, H.T, V, W)
-            Kinf = Pinf @ (H.T) @ (np.linalg.inv(H @ Pinf @ H.T + W))
-            Finf = np.linalg.inv(R_m + B.T @ Uinf @ B) @ B.T @ Uinf @ A
+        u = np.zeros((2,N))
+        Uinf = DARE(A,B,Q, R_m)
+        Pinf = DARE(A.T,H.T,V, W)
+        Kinf = Pinf@(H.T) @ (np.linalg.inv( H@Pinf@H.T + W))
+        Finf = np.linalg.inv(R_m + B.T@Uinf@B) @ B.T@Uinf@A
 
-            #x[:,k+1]=A@x[:,k]
-            x[:,k]=A@(x[:,k-1]) - B@( Finf@(xhat[:,k-1]) )
-         
-            #xhat[:,k+1]=(A - B@Finf - Kinf@H@A)@(xhat[:,k]) + Kinf@H@A@(x[:,k])
-            xhat[:,k]=sys_sol_lin.y[0][k-1]
-
-#         print(sys_sol_lin.y[1])
-#         plt.plot(x[2,:])
-#         plt.plot(sys_sol_lin.y[2])
-#         plt.show()
-#         
-#         plt.plot(x[1,:])
-#         plt.plot(sys_sol_lin.y[1])
-#         plt.show()
         
-#         plt.plot(x[0,:])
-#         plt.plot(sys_sol_lin.y[0])
-#         plt.show()
-        # Convert to 2D cartesian coordinates centered at earth's core
+        for k in range(N-1):
+            x[:,k+1]=A@(x[:,k]) - B@( Finf@(xhat[:,k]) )
+            #this is the formula for xhat given in Chapter 15 of 231B notes
+            #xhat[:,k+1]=(A - B@Finf - Kinf@H@A)@(xhat[:,k]) + Kinf@H@A@(x[:,k])
+            #using sys_sol_lin as a "measurement"
+            xhat[:,k+1]=sys_sol_lin.y[:,k]
+            
+        # Simulate discrete nonlinear system
+        x_discrete = np.zeros((len(t_sim)+1, 4))
+        x_discrete[0] = x0
+        for k in range(len(t_sim)):
+            x_discrete[k+1,:] = lin_dyn_discrete(x_discrete[k,:], r0, Ts)
+
+        for i in range(len(sys_sol_lin.y)):
+            sys_sol_lin.y[i] = np.array(
+                [sys_sol_lin.y[i][j]
+                 + equil(t_sim[j])[i] for j in range(len(t_sim))])
+            
+
+        # Add equilibrium values for discrete system
+        for k in range(len(x_discrete)):
+            x_discrete[k] = x_discrete[k] + equil(k*Ts)
+            
+        #Add equilibrium values for filtered and controlled system
+        for k in range(N):
+            x[:,k] = x[:,k] + equil(k*Ts)
+#         # Convert to 2D cartesian coordinates centered at earth's core
         x_sat_KF = [x[0][i]*np.cos(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
         y_sat_KF = [x[0][i]*np.sin(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
+
+#         x_sat_lin = [sys_sol_lin.y[0][i]*np.cos(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
+#         y_sat_lin = [sys_sol_lin.y[0][i]*np.sin(sys_sol_lin.y[2][i]) for i in range(len(t_sim))]
+#         # x_sat_nl = [sys_sol_nl.y[0][i]*np.cos(sys_sol_nl.y[2][i]) for i in range(len(t_sim))]
+        # y_sat_nl = [sys_sol_nl.y[0][i]*np.sin(sys_sol_nl.y[2][i]) for i in range(len(t_sim))]
+        x_sat_lin_discrete = [x_discrete[k][0]*np.cos(x_discrete[k][2]) for k in range(len(x_discrete))]
+        y_sat_lin_discrete = [x_discrete[k][0]*np.sin(x_discrete[k][2]) for k in range(len(x_discrete))]
+
         #  Plotting
         fig, ax = plt.subplots()
         circle1 = plt.Circle((0, 0), R, color='b')
         ax.add_artist(circle1)
-
         ax.plot(x_sat_KF, y_sat_KF, linewidth=4, color='r', linestyle='-')
+        ax.plot(x_sat_lin_discrete, y_sat_lin_discrete, linewidth=4, color='g',
+                linestyle='--')
         # plt.plot(x_sat_nl, y_sat_nl, linewidth=2)
         plt.xlabel('x')
         plt.ylabel('y')
@@ -369,6 +367,8 @@ def main():
         fig, ax = plt.subplots()
         circle1 = plt.Circle((0, 0), R, color='b')
         ax.add_artist(circle1)
+        plt.legend(['KF Model', 'Discrete Linear Model'],
+                   loc='lower right')
         plt.show()
         '''
         # Main code and simulation here
